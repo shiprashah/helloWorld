367bac443b23bc6fb6218ffd51fc595c123503b7
shipraar <shiprakumar31@gmail.com>
2016-07-17 09:29:53 -0500

    new file

---
+++ namasteJi.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+public class namasteJi {
+
+	public static void main(String[] args) {
+		System.out.println("Namaste Ji");
+		namasteJi nam=new namasteJi();
+		nam.test();
+
+	}
+
+	private void test() {
+		System.out.println(" Duniya ");
+		
+	}
+
+}
